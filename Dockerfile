528ee7bebc829a3496caae5153f3c1c38bade950
exurd <7785022+exurd@users.noreply.github.com>
2026-08-23 00:26:27 +0100

    use docker to compile the wasm

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# docker build -o . .
+FROM emscripten/emsdk:latest AS builder
+
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    autoconf \
+    automake \
+    libtool \
+    nodejs \
+    npm \
+    && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /src
+COPY . .
+
+RUN npm install && ./tools/build.sh
+
+FROM scratch AS exporter
+
+COPY --from=builder /src/libtimidity.wasm /
+COPY --from=builder /src/libtimidity.js /
+COPY --from=builder /src/libtimidity.debug.wasm /
+COPY --from=builder /src/libtimidity.debug.wasm.map /
+COPY --from=builder /src/libtimidity.debug.js /
--- tools/build.sh
+++ tools/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Compile the libtimidity C codebase to JavaScript with emscripten
-BUILD_FLAGS="-s STRICT=1 -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME=LibTimidity -s EXPORTED_FUNCTIONS=@tools/exports.json -s EXTRA_EXPORTED_RUNTIME_METHODS=@tools/exports-runtime.json -s FORCE_FILESYSTEM=1"
+BUILD_FLAGS="-s STRICT=1 -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s EXPORT_NAME=LibTimidity -s EXPORTED_FUNCTIONS=@tools/exports.json -s EXPORTED_RUNTIME_METHODS=@tools/exports-runtime.json -s FORCE_FILESYSTEM=1"
 
 # Maximize optimization options for smallest file size
 OPTIMIZE_FLAGS_PROD="-Oz -s ENVIRONMENT=web"
@@ -15,5 +15,5 @@ emcc -o libtimidity.debug.js $OPTIMIZE_FLAGS_DEBUG $BUILD_FLAGS libtimidity/src/
 # Include the freepats config in the published package so `brfs` can inline it
 cp node_modules/freepats/freepats.cfg freepats.cfg
 
-# Enhance the source maps for libtimidity.debug.js
-node tools/enhance-source-map.js
+# # Enhance the source maps for libtimidity.debug.js
+# node tools/enhance-source-map.js
